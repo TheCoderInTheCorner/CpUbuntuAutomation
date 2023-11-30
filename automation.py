6d6b6d935326e0dfc22316600b6ce42576e6ca92
TheCoderInTheCorner <109489785+TheCoderInTheCorner@users.noreply.github.com>
2023-11-29 20:32:26 -0500

    Update automation.py
    Way better still in progress

--- automation.py
+++ automation.py
@@ -1,68 +1,84 @@
 import subprocess
-#Proof Of GIT
-def filterUsers(authUsers_):
-    authUsers = authUsers_
-    authUsers = authUsers.split(",")
+'''
 
-    allusers = subprocess.run("ls -d */",shell=True,stdout=subprocess.PIPE,text=True)
-    allusers = allusers.stdout
-    allusers = allusers.split("/")
+Created by Basu Khadka Tue Nov 28 4:52 PM
+Basic Automation file for ubuntu 
+ 
+This Program will do the follwoing
 
-    for i in allusers:
-        processedData = allusers[allusers.index(i)].strip(" \n")
-        allusers[allusers.index(i)] = processedData
-    for i in authUsers:
-        processedData = authUsers[authUsers.index(i)].strip(" \n")
-        authUsers[authUsers.index(i)] = processedData
-    if(allusers.__contains__("")):
-        allusers.remove("")
-    authUsers=set(authUsers)
-    allusers=set(allusers)
-    for i in allusers:
-        x = 'sudo chmod +rwx '+i
-        subprocess.run(x,shell=True)
-    return allusers.difference(authUsers)
-# YOU CHANGE THIS LINE
-UsersDel=filterUsers('''Line You Change''')
-# ========================================
-print("Running Cyberpatriots Ubuntu Hardening")
-print("This program will notify you when deleting files, users or making any other significant changes")
+ [1] Updates
+ [2] UFW config
+ [3] Delete unwanted media files
+ [4] NGINX server is closed
+ [5] SSH is started and running
+ [6] Auditd is installed and running
+ [7] delete some common hacking tools
+ [8] sudo authentication
+ [9] Disbale root acess through SSHD
+ [10] Insecure Permissions on shadow file
+ [10] PAM files
 
-allow = input('Run Program (y/n)')
+'''
+
+#===============  Variables You Set Up ==============#
+AllUsers = [
+    ""
+]
+#====================================================#
 
 
-subprocess.run('sudo apt upgrade',shell=True)
 
-# firewall policies 
-subprocess.run('sudo ufw default deny incoming',shell=True)
-subprocess.run('sudo ufw default allow outgoing',shell=True)
-subprocess.run('sudo ufw enable',shell=True)
+#=============  Functions ============#
 
-# finds and deletes mp3 files, 
-# the + terminates the exec command and is more efficent as it allows the command to ground files together
-# NOTE: you can also use (\;) but this will run rm command for every file found instead of just grouping 
-# files together
-# the exec atribute just says to do the following with the files found 
-# and the {} is used to symbolize the files found
-# the -o stands for the OR operand
-
-print("========== Following files found in home directory: ==========")
-subprocess.run('sudo find /home/ \( -iname "*.mp3" -o -iname "*.mp4" -o -iname "*.mov" -o -iname "*.wav" \)',shell=True)
-
-delMedia = input("Delete All Media Files (y/n)")
-
-if(delMedia == 'y'):
-    subprocess.run(['sudo find /home/ \( -iname "*.mp3" -o -iname "*.mp4" -o -iname "*.mov" -o -iname "*.wav" \) -exec rm {} +'],shell=True)
-else:
-   print("Files Were Not Deleted")
-
-#Deleting Unauthorized users
-#Set parameters by yoursel
-for i in UsersDel:
-    grantAcess = input("Delete User: "+i+" (y/n)")
-    if(grantAcess == "y"):
-        x = "sudo userdel " + i + " -fr"
-        if(i != ""): #YOU PUT IN YOUR NAME
-            subprocess.run(x,shell=True)
+def returnCode(command,Passed,Failed):
+    if(command.returncode==0):
+        print("-"*20)
+        print(Passed)
+        print("-"*20)
     else:
-        print("Did not delete User "+i)
+        print("!"*20)
+        print(Failed)
+        print("!"*20)
+    
+def GetProhibitedFiles(rmv:bool):
+    files = subprocess.run("sudo find home \( -iname '*.mp3' -o -iname '*.mp4' -o -iname '*.mov' -o -iname '*.wav' \)",shell=True,capture_output=True,text=True)
+    if(not rmv):
+        return files.stdout
+    if(rmv):
+        deletedFiles=subprocess.run("sudo find home \( -iname '*.mp3' -o -iname '*.mp4' -o -iname '*.mov' -o -iname '*.wav' \) -exec rm {} + >/dev/null",shell=True)
+        returnCode(deletedFiles,"Succesfully Deleted Files","FAILED to delete files")
+
+#=====================================#
+
+
+# Updates
+subprocess.run("sudo apt upgrade",shell=True)
+subprocess.run("sudo apt update",shell=True)
+
+# UFW Config
+subprocess.run("sudo apt install ufw",shell=True)
+deny = subprocess.run("sudo ufw default deny incoming >/dev/null",shell=True)
+
+returnCode(deny,"UFW Incoming is set to deny","UFW Incoming FAILED to set to deny")
+allow = subprocess.run("sudo ufw default allow outgoing >/dev/null",shell=True)
+returnCode(allow,"UFW outgoing is set to allow","UFW outgoing FAILED to set to allow")
+
+enable = subprocess.run("sudo ufw enable >/dev/null",shell=True)
+returnCode(enable,"UFW enabled","UFW FAILED TO ENABLE")
+
+# Media Files
+files = GetProhibitedFiles(False)
+print("==================== Prohibited Files ====================\n")
+print(files)
+print("==========================================================")
+delete = input("Would You Like To Delete These Prohibited Files? (y/n)")
+if(delete == "y"):
+    GetProhibitedFiles(True)
+else:
+    print("="*20 + "\nDid not delete FIles\n" + "="*20)
+
+# NGINX Service
+subprocess.run("sudo systemctl stop nginx",shell=True)
+subprocess.run("sudo systemctl disable nginx",shell=True)
+
+# SSH
